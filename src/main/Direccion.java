6fc13a13ede910773d90007d48ac0a24d20b3383
migfe <migfe@192.168.40.46>
2018-04-13 12:37:45 -0300

    cree la wea

--- src/main/Direccion.java
+++ src/main/Direccion.java
@@ -11,4 +11,29 @@ package main;
  */
 public class Direccion {
     
+    private String nombre;
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre= nombre;
+    }
+    
+    private int numero;
+    public int getnumero(){
+        return numero;
+        
+    }
+    public void setNumero(int numero){
+        this.numero= numero ;
+    }
+
+    public Direccion(String nombre, int numero) {
+        this.nombre = nombre;
+        this.numero = numero;
+    }
+            
+    
+    
+    
 }
